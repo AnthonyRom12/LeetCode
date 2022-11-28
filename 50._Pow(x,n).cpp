9a199cea79a15017c3acd7b9730aedb80c3986ed
Anton <106614976+AnthonyRom12@users.noreply.github.com>
2022-11-28 23:02:25 +0300

    Create 50._Pow(x,n).cpp

---
+++ 50._Pow(x,n).cpp
@@ -0,0 +1,27 @@
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        if (n < 0) {
+            x = 1 / x;
+        }
+
+        long num = labs(n);
+
+        double pow = 1;
+
+        while (num) { // equivalent to while(num != 0)
+            if (num & 1) { // equivalent to if((num & 1) != 0)
+                pow *= x;
+            }
+
+            x *= x;
+            num >>= 1;
+        }
+
+        return pow;
+    }
+};
