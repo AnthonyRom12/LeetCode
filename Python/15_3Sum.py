cba642ebf8c3ffe0101621206bc38690334a00f8
Anton <106614976+AnthonyRom12@users.noreply.github.com>
2023-08-31 13:44:52 +0300

    Create 15_3Sum.py

---
+++ Python/15_3Sum.py
@@ -0,0 +1,56 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+"""
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # sorting
+        nums.sort()
+        result = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # avoid duplicates
+
+            left, right = i + 1, len(nums) - 1  # Initialize pointers
+
+            while left < right:  # Two-pointers approach
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:  # check sum
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1  # Avoid Duplicates
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1  # Avoid Duplicates
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
