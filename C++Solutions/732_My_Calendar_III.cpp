9f96411f03a95cdd25afb181eebb812ee292e5cc
Anton <106614976+AnthonyRom12@users.noreply.github.com>
2023-09-14 15:23:06 +0300

    Create 732_My_Calendar_III.cpp

---
+++ C++Solutions/732_My_Calendar_III.cpp
@@ -0,0 +1,42 @@
+/*
+A k-booking happens when k events have some non-empty intersection (i.e., there is some time that is common to all k events.)
+
+You are given some events [startTime, endTime), after each given event, return an integer k representing the maximum k-booking between all the previous events.
+
+Implement the MyCalendarThree class:
+
+MyCalendarThree() Initializes the object.
+int book(int startTime, int endTime) Returns an integer k representing the largest integer such that there exists a k-booking in the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendarThree", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, 1, 1, 2, 3, 3, 3]
+
+Explanation
+MyCalendarThree myCalendarThree = new MyCalendarThree();
+myCalendarThree.book(10, 20); // return 1
+myCalendarThree.book(50, 60); // return 1
+myCalendarThree.book(10, 40); // return 2
+myCalendarThree.book(5, 15); // return 3
+myCalendarThree.book(5, 10); // return 3
+myCalendarThree.book(25, 55); // return 3
+*/
+
+class MyCalendarThree {
+    map<int, int> times;
+public:
+    int book(int startTime, int endTime) {
+        times[startTime]++;
+        times[endTime]--;
+        int event_on = 0, ans = 0;
+        for (pair<int, int> x: times) {
+            ans = max(ans, event_on += x.second);
+        }
+        return ans;
+    }
+};
