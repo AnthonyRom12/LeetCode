55f35149311463a764ac4062aee9fbcf2d6fce43
Anton <106614976+AnthonyRom12@users.noreply.github.com>
2023-09-24 00:09:57 +0300

    Create 2828_Check_if_a_String_Is_an_Acronym_of_Words.cpp

---
+++ C++Solutions/2828_Check_if_a_String_Is_an_Acronym_of_Words.cpp
@@ -0,0 +1,55 @@
+/*
+Given an array of strings words and a string s, determine if s is an acronym of words.
+
+The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+Return true if s is an acronym of words, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: words = ["alice","bob","charlie"], s = "abc"
+Output: true
+Explanation: The first character in the words "alice", "bob", and "charlie" are 'a', 'b', and 'c', respectively. Hence, s = "abc" is the acronym. 
+Example 2:
+
+Input: words = ["an","apple"], s = "a"
+Output: false
+Explanation: The first character in the words "an" and "apple" are 'a' and 'a', respectively. 
+The acronym formed by concatenating these characters is "aa". 
+Hence, s = "a" is not the acronym.
+Example 3:
+
+Input: words = ["never","gonna","give","up","on","you"], s = "ngguoy"
+Output: true
+Explanation: By concatenating the first character of the words in the array, we get the string "ngguoy". 
+Hence, s = "ngguoy" is the acronym.
+  */
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+class Solution {
+public:
+    bool isAcronym(std::vector<std::string>& words, std::string s) {
+        
+        if (words.empty()) {
+            return s.empty();
+        }
+        
+       
+        std::string acronym;
+        for (const std::string& word : words) {
+            acronym += word[0];
+        }
+        
+        
+        std::transform(acronym.begin(), acronym.end(), acronym.begin(), ::tolower);
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+        
+        return acronym == s;
+    }
+};
